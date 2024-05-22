aff0fa85bd7613b6031772b5f1bad06a7218faf0
刘靖煌 <liujinghuang@migu.cn>
2024-05-22 18:16:06 +0800

    生成Xcode工程了

---
+++ xcode/CMakeFiles/3.29.3/CompilerIdCXX/CompilerIdCXX.build/Debug/CompilerIdCXX.build/Objects-normal/x86_64/CMakeCXXCompilerId.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/liujinghaung/Documents/Github/Computer\ Graphics/LearnOpenGLDemo/xcode/CMakeFiles/3.29.3/CompilerIdCXX/CMakeCXXCompilerId.cpp
---
+++ xcode/configuration/root_directory.h
@@ -0,0 +1,1 @@
+const char * logl_root = "/Users/liujinghaung/Documents/Github/Computer Graphics/LearnOpenGLDemo";
